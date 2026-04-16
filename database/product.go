de129bd780e789722b0621de62d93c4e7c6743f3
Shuayeb-X <hasan15-5673@diu.edu.bd>
2026-04-16 04:43:26 -0400

    Moved product packaged all file into database

---
+++ database/product.go
@@ -0,0 +1,11 @@
+package database
+
+var Productlist []Product
+
+type Product struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Price       string `json:"price"`
+	ImgUrl      string `json:"imgUrl"`
+}
