fed62440b563219df034a51bfe9e22df311049dd
Shuayeb Hasan <hasan15-5673@diu.edu.bd>
2026-04-16 04:44:53 -0400

    added  product

--- database/product.go
+++ database/product.go
@@ -9,3 +9,15 @@ type Product struct {
 	Price       string `json:"price"`
 	ImgUrl      string `json:"imgUrl"`
 }
+
+func init() {
+	prd1 := Product{
+		Id:          1,
+		Title:       "Dragon",
+		Description: "Dragon is red",
+		Price:       "200 tk",
+		ImgUrl:      "https://www.tastingtable.com/img/gallery/how-to-eat-dragon-fruit-for-the-uninitiated/intro-1682966430.jpg",
+	}
+
+	Productlist = append(Productlist, prd1)
+}
